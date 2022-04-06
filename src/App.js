41f1159b60c170115fb843d83bc56246f98d88d2
msobe <msobe@gmail.com>
2022-04-06 13:08:40 +0200

    updated/fixed auth

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <AmplifySignOut />
-    </div>
+      <Authenticator>
+          { ({ signOut, user }) => (
+              <div className="App">
+                  <header className="App-header">
+                      <img src={logo} className="App-logo" alt="logo" />
+                      <h1>Hello from V2</h1>
+                      <p>
+                          Edit <code>src/App.js</code> and save to reload.
+                      </p>
+                      <a
+                          className="App-link"
+                          href="https://reactjs.org"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                      >
+                          Learn React
+                      </a>
+                      <button onClick={signOut}>Sign out</button>
+                  </header>
+              </div>
+          )}
+      </Authenticator>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
